95757ef0db7e089b344cb70364d4ba4debdcb443
suchitavavhal <97341168+suchitavavhal@users.noreply.github.com>
2022-12-08 19:19:47 +0530

    Chrome Extension

---
+++ ChromeExt/script.js
@@ -0,0 +1,17 @@
+async function fetchData(){
+    const res= await fetch("https://api.coronavirus.data.gov.uk/v1/data");
+    const record = await res.json();
+    document.getElementById("date").innerHTML=record.data[0].date;
+    document.getElementById("areaName").innerHTML=record.data[0].areaName;
+    document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
+    document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+
+    // displaying the data in json
+
+    // for(let i=0; i< record.data.length; i++){
+
+    //     console.log(record.data[i]); 
+    // }
+    console.log(record.data);
+}
+fetchData();
